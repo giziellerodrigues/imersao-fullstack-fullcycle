f08396489eb0af122cfb153a8ff30babb9c219d0
Gizielle Rodrigues <giziellecinttia@gmail.com>
2021-02-02 16:33:56 -0300

    Criacao nov model

---
+++ domain/model/user_test.go
@@ -0,0 +1,26 @@
+package model_test
+
+import (
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/codeedu/imersao/codepix-go/domain/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestModel_NewUser(t *testing.T) {
+
+	email := "joao@provedor.com"
+	name := "Joao"
+
+	user, err := model.NewUser(name, email)
+
+	require.Nil(t, err)
+	require.NotEmpty(t, uuid.FromStringOrNil(user.ID))
+	require.Equal(t, user.Name, name)
+	require.Equal(t, user.Email, email)
+
+	_, err = model.NewUser("", "")
+	require.NotNil(t, err)
+}
